098d849d3320b91b2f942d8fb0e166fb214ff855
Oren Ben-Meir <orenbm11@gmail.com>
2023-02-02 18:33:01 -0500

    clean internal topics

--- src/main/java/com/example/kafkastreams/instrument/adminjob/TopicsDeleter.kt
+++ src/main/java/com/example/kafkastreams/instrument/adminjob/TopicsDeleter.kt
@@ -2,6 +2,7 @@ package com.example.kafkastreams.instrument.adminjob
 
 import com.example.kafkastreams.loadProperties
 import org.apache.kafka.clients.admin.Admin
+import org.apache.kafka.clients.admin.ListTopicsOptions
 import java.io.IOException
 
 fun main() {
@@ -11,18 +12,8 @@ fun main() {
 fun runDeleter() {
     val properties = loadProperties()
     Admin.create(properties).use { adminClient ->
-        val inputTopicA = properties.getProperty("instrument_a.input.topic")
-        val inputTopicB = properties.getProperty("instrument_b.input.topic")
-        val inputTopicC = properties.getProperty("instrument_c.input.topic")
-        val inputTopicD = properties.getProperty("instrument_d.input.topic")
-        val inputTopicE = properties.getProperty("instrument_e.input.topic")
-        val outputTopicA = properties.getProperty("instrument_a.output.topic")
-        val outputTopicB = properties.getProperty("instrument_b.output.topic")
-        val outputTopicC = properties.getProperty("instrument_c.output.topic")
-        val topics = listOf(
-            inputTopicA, inputTopicB, inputTopicD,
-            inputTopicC, inputTopicE, outputTopicA, outputTopicB, outputTopicC
-        )
+        val listTopicOpts = ListTopicsOptions().listInternal(true)
+        val topics = adminClient.listTopics(listTopicOpts).names().get()
         adminClient.deleteTopics(topics)
     }
 }
